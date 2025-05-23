6d7b0abdc84d0a2d7e813bf3510dadc90551eb0a
Shuvam Gupta <shubhamguptakmr@gmail.com>
2025-05-23 23:44:53 +0530

    first1

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/home', (req, res) => {
+  console.log('API is running home...');
+  res.send('Home route response from backend');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
